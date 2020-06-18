d2cf7c71ade99e5c50fde1c8491c78877c8fe754
Zh <zeming@dreamofsky.com>
2020-06-18 16:47:14 -0400

    add touch drag controls

--- src/components/Dial.vue
+++ src/components/Dial.vue
@@ -3,6 +3,7 @@
     :id="dialId + 'Dial'"
     class="dial"
     @mousedown="onDrag"
+    @touchstart="onDrag"
     :style="`transform: rotate(${rotation}deg)`"
   >
     <span
@@ -15,6 +16,9 @@
 
 <script>
 import { mapState, mapMutations } from 'vuex';
+import geometery from '../logic/geometery';
+
+const { findRotation } = geometery;
 
 export default {
   name: 'Dial',
@@ -38,16 +42,27 @@ export default {
     }
   },
   methods: {
-    ...mapMutations(['dragStart', 'dragEnd']),
+    ...mapMutations(['dragStart', 'dragEnd', 'dragEnter']),
     onDrag() {
       this.dragStart(this.dialId);
-      window.addEventListener('mouseup', this.doneDrag);
-      window.addEventListener('touchend', this.doneDrag);
+      document.addEventListener('mouseup', this.doneDrag);
+      document.addEventListener('touchend', this.doneDrag);
+      document.addEventListener('touchmove', this.touchMoveHandler);
+    },
+    touchMoveHandler(evt) {
+      const x = evt.changedTouches[0].pageX;
+      const y = evt.changedTouches[0].pageY;
+      const element = document.elementFromPoint(x, y);
+      if (element && element.parentNode) {
+        const deg = findRotation(element.parentNode);
+        this.dragEnter(deg);
+      }
     },
     doneDrag() {
       this.dragEnd();
-      window.removeEventListener('mouseup', this.dragEnd);
-      window.removeEventListener('touchend', this.doneDrag);
+      document.removeEventListener('mouseup', this.dragEnd);
+      document.removeEventListener('touchend', this.doneDrag);
+      document.removeEventListener('touchmove', this.touchMoveHandler);
     }
   }
 };
